d63d6d4a35424fce7ae66d897ca27ff4170cf14b
Richard Chea <rchea817@gmail.com>
2020-04-09 21:01:22 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,17 @@
+require "pry"
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  collection.each do |list|
+    # binding.pry
+    if name == list[:item]
+      # binding.pry
+      return list
+      # binding.pry
+    end
+  end
+  nil
 end
 
 def consolidate_cart(cart)
@@ -10,8 +19,20 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  checkout_cart = []
+  cart.each do |cart_item|
+    item_name = cart_item[:item]
+    item_info = find_item_by_name_in_collection(item_name, checkout_cart)
+    if item_info
+      item_info[:count] += 1
+    else
+      checkout_cart << {
+        :item => item_name,
+        :price => cart_item[:price],
+        :clearance => cart_item[:clearance],
+        :count => 1
+      }
+    end
+  end
+  checkout_cart
 end
-
-
-  
